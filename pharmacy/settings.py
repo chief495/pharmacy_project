e5a854f153853372590315a8515d24676f37c58d
Eduard <ednest2000@gmail.com>
2026-01-19 22:01:54 +0500

    Исправил ошибки, вощникшие при деплое

--- pharmacy/settings.py
+++ pharmacy/settings.py
@@ -6,17 +6,18 @@ from decouple import config
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
+CSRF_TRUSTED_ORIGINS = [
+    f"https://{ALLOWED_HOSTS[0]}",
+    f"http://{ALLOWED_HOSTS[0]}",
+]
+# Добавьте localhost при DEBUG
+if DEBUG:
+    ALLOWED_HOSTS.extend(['localhost', '127.0.0.1'])
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-
 # Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -49,6 +50,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.static",  # Добавьте эту строку!
             ],
         },
     },
@@ -57,8 +59,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "pharmacy.wsgi.application"
 
 # Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -75,25 +75,25 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",  # Стандартный backend (для админки)
 ]
 
-# Authentication URLs
-LOGIN_URL = "login"
+# Authentication URLs - ИСПРАВЬТЕ с namespace
+LOGIN_URL = "drugs:login"  # Добавьте namespace!
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
 # Email settings
 EMAIL_BACKEND = config(
-    "EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend"
+    "EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend"
 )
 EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
 EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
+EMAIL_USE_SSL = config("EMAIL_USE_TLS", default=True, cast=bool)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="noreply@pharmamonitor.com")
+DEFAULT_FROM_EMAIL=EMAIL_HOST_USER
+EMAIL_TIMEOUT = 30  # Таймаут в секундах
+EMAIL_USE_LOCALTIME = True
 
 # Password validation
-# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -115,21 +115,22 @@ TIME_ZONE = "Europe/Moscow"
 USE_I18N = True
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
+# Static files - ИСПРАВЬТЕ ЭТО!
 STATIC_URL = "/static/"
-# Эта папка создастся автоматически при команде collectstatic
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# Ваши исходные статические файлы
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),  # Ваши файлы здесь
+]
+
+# Папка, куда собираются ВСЕ статические файлы (включая админку)
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # ИЗМЕНИТЕ НА staticfiles!
 
 # Media files
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGGING = {
@@ -145,3 +146,9 @@ LOGGING = {
         "level": "DEBUG",
     },
 }
+
+# Временно отключите security settings для разработки
+if DEBUG:
+    SECURE_SSL_REDIRECT = False
+    SESSION_COOKIE_SECURE = False
+    CSRF_COOKIE_SECURE = False
